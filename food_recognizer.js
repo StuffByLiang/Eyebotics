d6638aba4d40de51156c284f62ea579724052b13
annieliu10 <annieyt8880@gmail.com>
2021-01-16 17:13:22 +0800

    debugged

--- food_recognizer.js
+++ food_recognizer.js
@@ -3,9 +3,6 @@
 
 // key DOM elements (select by id)
 
-// Store the resulting model in the global scope of our app.
-var model = undefined;
-
 const video = document.getElementById("webcam");
 const liveView = document.getElementById("liveView");
 const demosSection = document.getElementById("demos");
@@ -44,6 +41,9 @@ function enableCam(event) {
   });
 }
 
+// Store the resulting model in the global scope of our app.
+var model = undefined;
+
 // Before we can use COCO-SSD class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment
 // to get everything needed to run.
@@ -55,72 +55,72 @@ cocoSsd.load().then(function (loadedModel) {
   demosSection.classList.remove("invisible");
 });
 
-// var children = [];
+var children = [];
 
-// function predictWebcam() {
-//   // Now let's start classifying a frame in the stream.
-//   model.detect(video).then(function (predictions) {
-//     // Remove any highlighting we did previous frame.
-//     for (let i = 0; i < children.length; i++) {
-//       liveView.removeChild(children[i]);
-//     }
-//     children.splice(0);
+function predictWebcam() {
+  // Now let's start classifying a frame in the stream.
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
 
-//     // Now lets loop through predictions and draw them to the live view if
-//     // they have a high confidence score.
-//     for (let n = 0; n < predictions.length; n++) {
-//       // If we are over 66% sure we are sure we classified it right, draw it!
-//       if (predictions[n].score > 0.66) {
-//         const p = document.createElement("p");
-//         p.innerText =
-//           predictions[n].class +
-//           " - with " +
-//           Math.round(parseFloat(predictions[n].score) * 100) +
-//           "% confidence.";
-//         p.style =
-//           "margin-left: " +
-//           predictions[n].bbox[0] +
-//           "px; margin-top: " +
-//           (predictions[n].bbox[1] - 10) +
-//           "px; width: " +
-//           (predictions[n].bbox[2] - 10) +
-//           "px; top: 0; left: 0;";
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement("p");
+        p.innerText =
+          predictions[n].class +
+          " - with " +
+          Math.round(parseFloat(predictions[n].score) * 100) +
+          "% confidence.";
+        p.style =
+          "margin-left: " +
+          predictions[n].bbox[0] +
+          "px; margin-top: " +
+          (predictions[n].bbox[1] - 10) +
+          "px; width: " +
+          (predictions[n].bbox[2] - 10) +
+          "px; top: 0; left: 0;";
 
-//         const highlighter = document.createElement("div");
-//         highlighter.setAttribute("class", "highlighter");
-//         highlighter.style =
-//           "left: " +
-//           predictions[n].bbox[0] +
-//           "px; top: " +
-//           predictions[n].bbox[1] +
-//           "px; width: " +
-//           predictions[n].bbox[2] +
-//           "px; height: " +
-//           predictions[n].bbox[3] +
-//           "px;";
+        const highlighter = document.createElement("div");
+        highlighter.setAttribute("class", "highlighter");
+        highlighter.style =
+          "left: " +
+          predictions[n].bbox[0] +
+          "px; top: " +
+          predictions[n].bbox[1] +
+          "px; width: " +
+          predictions[n].bbox[2] +
+          "px; height: " +
+          predictions[n].bbox[3] +
+          "px;";
 
-//         liveView.appendChild(highlighter);
-//         liveView.appendChild(p);
-//         children.push(highlighter);
-//         children.push(p);
-//       }
-//     }
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
 
-//     // Call this function again to keep predicting when the browser is ready.
-//     window.requestAnimationFrame(predictWebcam);
-//   });
-// }
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
 
-// // Store the resulting model in the global scope of our app.
-// var model = undefined;
+// Store the resulting model in the global scope of our app.
+var model = undefined;
 
-// // Before we can use COCO-SSD class we must wait for it to finish
-// // loading. Machine Learning models can be large and take a moment
-// // to get everything needed to run.
-// // Note: cocoSsd is an external object loaded from our index.html
-// // script tag import so ignore any warning in Glitch.
-// cocoSsd.load().then(function (loadedModel) {
-//   model = loadedModel;
-//   // Show demo section now model is ready to use.
-//   demosSection.classList.remove("invisible");
-// });
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment
+// to get everything needed to run.
+// Note: cocoSsd is an external object loaded from our index.html
+// script tag import so ignore any warning in Glitch.
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove("invisible");
+});
