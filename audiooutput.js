be805e48cff92d764e18efe2e37e02c744d4fef5
annieliu10 <annieyt8880@gmail.com>
2021-01-17 03:51:59 +0800

    add audio output

--- audiooutput.js
+++ audiooutput.js
@@ -1,1 +1,33 @@
 // audio output for the name of the food
+// Imports the Google Cloud client library
+const textToSpeech = require("@google-cloud/text-to-speech");
+
+// Import other required libraries
+const fs = require("fs");
+const util = require("util");
+
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
+
+async function quickStart() {
+  // The text to synthesize
+  const text = "hello, world!";
+
+  // Construct the request
+  const request = {
+    input: { text: text },
+    // Select the language and SSML voice gender (optional)
+    voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
+    // select the type of audio encoding
+    audioConfig: { audioEncoding: "MP3" },
+  };
+
+  // Performs the text-to-speech request
+  //what's in .then executes after the method .then is chained to finishes
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile("output.mp3", response.audioContent, "binary");
+  console.log("Audio content written to file: output.mp3");
+}
+quickStart();
--- food_recognizer.js
+++ food_recognizer.js
@@ -1,5 +1,17 @@
+// audio output for the name of the food
+// Imports the Google Cloud client library
+
 // const status = document.getElementById("status");
 // status.innerText = "Loaded TensorFlow.js - version: " + tf.version.tfjs;
+
+// Imports the Google Cloud client library
+const textToSpeech = require("@google-cloud/text-to-speech");
+
+// Import other required libraries
+const fs = require("fs");
+const util = require("util");
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
 
 // key DOM elements (select by id)
 
@@ -59,7 +71,7 @@ var children = [];
 
 function predictWebcam() {
   // Now let's start classifying a frame in the stream.
-  model.detect(video).then(function (predictions) {
+  model.detect(video).then(async function (predictions) {
     // Remove any highlighting we did previous frame.
     for (let i = 0; i < children.length; i++) {
       liveView.removeChild(children[i]);
@@ -103,6 +115,7 @@ function predictWebcam() {
         liveView.appendChild(p);
         children.push(highlighter);
         children.push(p);
+        await quickStart(predictions[n].class);
       }
     }
 
@@ -111,16 +124,25 @@ function predictWebcam() {
   });
 }
 
-// Store the resulting model in the global scope of our app.
-var model = undefined;
+async function quickStart(text) {
+  // The text to synthesize
 
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove("invisible");
-});
+  // Construct the request
+  const request = {
+    input: { text: text },
+    // Select the language and SSML voice gender (optional)
+    voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
+    // select the type of audio encoding
+    audioConfig: { audioEncoding: "MP3" },
+  };
+
+  // Performs the text-to-speech request
+  //what's in .then executes after the method .then is chained to finishes
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile("output.mp3", response.audioContent, "binary");
+  var audio = new Audio("output.mp3");
+  audio.play();
+}
+
